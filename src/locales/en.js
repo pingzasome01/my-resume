5ac7c49d86a8d2632ab8924a861bd41ea3ac3007
pingpong <nattapon@datafirst.co.th>
2023-05-19 11:16:58 +0700

    Add new translations and update existing ones for the English and Thai versions of the website, including the addition of a personal profile section

--- src/locales/en.js
+++ src/locales/en.js
@@ -1,10 +1,27 @@
 export default {
-  Home: 'Home',
-  TVC: 'Video',
-  Product : 'Product information',
-  Customer : 'Customer reviews',
-  Location : 'Location',
-  Promotions : 'Promotions',
-  Application: 'Application download',
+  Navigator: {
+    Home: 'Home',
+    about: 'About Me',
+    tool: 'Tools',
+    contact: 'Contact Me',
+
+  },  
+  MyHome:{
+    Me:'Hello',
+    Nickname:'This is my personal website.',
+    subNickname:'I created this website for Let viewers view my profile and practice their own skills. If there is an error Sorry, I am a newbie.'
+  },
+  Myabout:{
+    head:'Graduated',
+    head1:'Experience',
+    head2:'Aptitude and interest',
+    graduated :'I graduated in Engineering My major Information Engineering  from  King Mongkut Institute of Technology Ladkrabang Prince of Chumphon Campus ',
+    experience1 :'Internship in cooperative education at IBM Solution Delivery Company in the position of IT Security for a period of 6 months.',
+    experience2 :'Made a mini project about thermometer showing the results on the website and Mobile Application during the 4th year of study.',
+    experience3 :'Currently working at Datafirst as Deverlopers.',
+    aptitude :'Currently, the work that is being done will be mainly in the front-end part. The passive skills that are available will be in the part of Design , css framework , vue.js(vite) (tailwind,bootstrap,vuetify) , animation on scroll. , Responsive Design ',
+    aptitude2 :'Like to work and ready to learn new things.'
+   
+  },
   
   }
--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import router from './router'
 import i18n from './plugins/i18n'
 
 import 'aos/dist/aos.css'
+import './assets/main.css'
 const app = createApp(App)
 
 app.use(i18n)
