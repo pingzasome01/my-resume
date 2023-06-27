217808f72d631f2aa16023d2e6eaae8274b4b4f4
pingpong <nattapon@datafirst.co.th>
2023-06-27 11:30:07 +0700

    Add vue3-lottie dependency and import it in main.js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 // import './assets/main.css'
-import 'tailwindcss/tailwind.css';
+import 'tailwindcss/tailwind.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
@@ -7,9 +7,12 @@ import i18n from './plugins/i18n'
 
 import 'aos/dist/aos.css'
 import './assets/main.css'
+import Vue3Lottie from 'vue3-lottie'
+import 'vue3-lottie/dist/style.css'
+
 const app = createApp(App)
 
 app.use(i18n)
 app.use(router)
-
+app.use(Vue3Lottie)
 app.mount('#app')
