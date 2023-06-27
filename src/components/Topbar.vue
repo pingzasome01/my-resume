23079c471e2d58f956022366486e8f7992e3f2f0
pingpong <nattapon@datafirst.co.th>
2023-06-27 11:30:32 +0700

    Add Topbar component and language switch functionality

---
+++ src/components/Topbar.vue
@@ -0,0 +1,42 @@
+<script setup>
+import { useI18n } from 'vue-i18n'
+
+const { locale } = useI18n()
+
+const change = (e) => {
+  locale.value = e
+}
+</script>
+<template>
+  <header class="text-gray-700 body-font border-b border-gray-200">
+    <div class="container mx-auto flex flex-wrap p-5 flex-col fixed z-10 md:flex-row items-center">
+      <a class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+        <img class="h-12 w-12 rounded-full" src="../assets/images/Lemon.jpg" alt="" />
+        <span class="ml-3 text-xl profilefont">My Personal Profile</span>
+      </a>
+      <nav class="md:ml-auto flex flex-wrap items-center text-base justify-center">
+        <a href="#Home" class="mr-5 hover:text-gray-900">{{ $t('Navigator.Home') }}</a>
+        <a href="#Aboutme" class="mr-5 hover:text-gray-900">{{ $t('Navigator.about') }}</a>
+        <a href="#Tools" class="mr-5 hover:text-gray-900">{{ $t('Navigator.tool') }}</a>
+        <a class="mr-5 hover:text-gray-900">{{ $t('Navigator.contact') }}</a>
+      </nav>
+      <div class="flex">
+        <img
+          class="w-9 h-9 p-1 mt-1"
+          src="../assets/images/flag/Thailand.png"
+          @click="change('th')"
+        />
+        <img
+          class="w-11 p-1 h-11"
+          src="../assets/images/flag/England-Flag-Transparent-Free-PNG.png"
+          @click="change('en')"
+        />
+      </div>
+      <!-- <button class="inline-flex items-center bg-gray-200 border-0 py-1 px-3 focus:outline-none hover:bg-gray-300 rounded text-base mt-4 md:mt-0">Button
+      <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-1" viewBox="0 0 24 24">
+        <path d="M5 12h14M12 5l7 7-7 7"></path>
+      </svg>
+    </button> -->
+    </div>
+  </header>
+</template>
--- src/locales/en.js
+++ src/locales/en.js
@@ -3,30 +3,32 @@ export default {
     Home: 'Home',
     about: 'About Me',
     tool: 'Tools',
-    contact: 'Contact Me',
-
-  },  
-  MyHome:{
-    Me:'Hell',
-    Nickname:'This is my personal website.',
-    subNickname:'I created this website for Let viewers view my profile and practice their own skills. If there is an error Sorry, I am a newbie.'
+    contact: 'Contact Me'
   },
-  Myabout:{
-    head:'Graduated',
-    head1:'Experience',
-    head2:'Aptitude and interest',
-    graduated :'I graduated in Engineering My major Information Engineering  from  King Mongkut Institute of Technology Ladkrabang Prince of Chumphon Campus ',
-    experience1 :'Internship in cooperative education at IBM Solution Delivery Company in the position of IT Security for a period of 6 months The intern project is an internal data management system side by side and data export as csv file.',
-    experience2 :'Made a mini project about thermometer showing the results on the website and Mobile Application during the 4th year of study.',
-    experience3 :'Currently working at Datafirst as Deverlopers.',
-    aptitude :'Currently, the work that is being done will be mainly in the front-end part. The passive skills that are available will be in the part of Design , css framework , vue.js(vite) (tailwind,bootstrap,vuetify) , animation on scroll. , Responsive Design ',
-    aptitude2 :'Like to work and ready to learn new things.'
-   
+  MyHome: {
+    Me: 'Hello',
+    Nickname: 'This is my personal website.',
+    subNickname:
+      'I created this website for Let viewers view my profile and practice their own skills. If there is an error Sorry, I am a newbie.'
   },
-  Myskill:{
-    headskill:'My Skill and Other',
-    title:'It is a skill that has been used in the workplace. used to some degree There are some that used to be used but not very good at it. So I did not put it in.'
-    
+  Myabout: {
+    head: 'Graduated',
+    head1: 'Experience',
+    head2: 'Aptitude and interest',
+    graduated:
+      'I graduated in Engineering My major Information Engineering  from  King Mongkut Institute of Technology Ladkrabang Prince of Chumphon Campus ',
+    experience1:
+      'Internship in cooperative education at IBM Solution Delivery Company in the position of IT Security for a period of 6 months The intern project is an internal data management system side by side and data export as csv file.',
+    experience2:
+      'Made a mini project about thermometer showing the results on the website and Mobile Application during the 4th year of study.',
+    experience3: 'Currently working at Datafirst as Deverlopers.',
+    aptitude:
+      'Currently, the work that is being done will be mainly in the front-end part. The passive skills that are available will be in the part of Design , css framework , vue.js(vite) (tailwind,bootstrap,vuetify) , animation on scroll. , Responsive Design ',
+    aptitude2: 'Like to work and ready to learn new things.'
+  },
+  Myskill: {
+    headskill: 'My Skill and Other',
+    title:
+      'It is a skill that has been used in the workplace. used to some degree There are some that used to be used but not very good at it. So I did not put it in.'
   }
-  
-  }
+}
